ab041cb66a95361a1492332a6482cb07784296f3
ryan <ryan.williams93@aol.com>
2022-01-07 18:42:44 -0600

    adjusting save commits

--- utils/generate-site.js
+++ utils/generate-site.js
@@ -44,3 +44,5 @@ module.exports = { writeFile, copyFile };
 //   };
 
 // This is known as using shorthand property names. So if we have a property key name with the same name as the value we're associating with it, like writeFile: writeFile, we can just say writeFile, and it will understand that we're using writeFile for both the property name and its value.
+
+// you can only use module.exports once per file
